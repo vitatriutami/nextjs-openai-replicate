709f555f3b8ffdc24627612dcbc3d679d50902a7
vitatriutami <vitatriutami@gmail.com>
2024-07-05 10:40:51 +0700

    rendering result

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,22 @@ export default function Home() {
         <input name="prompt" placeholder="Enter your prompt, ..." className="border-2 border-emerald-300 p-2 rounded-lg gap-4" />
         <button disabled={pending} className="bg-emerald-300 p-2 rounded-lg" >Generate Story</button>
       </form>
-      
+      {state?.success && (
+        <div>
+          <img alt={state.data.story.title} src={state.data.image} width={800} height={600} className="rounded-xl" />
+          <h3>{state.data.story.title}</h3>
+          <div>
+            {state.data.story.story.map((item, index) => {
+              return <div key={index}>{item}</div>;
+            })}
+          </div>
+          <div>
+            {state.data.story.moralLessons.map((item, index) => {
+              return <div key={index}>{item}</div>;
+            })}
+          </div>
+        </div>
+      )}
     </main>
   );
 }
